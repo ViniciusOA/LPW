7165dd5b5f5a5c2f77cb98e41ee8d542142873c1
Vinicius Oliveira de Almeida <vinicius.oliveiraalmeida1@gmail.com>
2019-02-21 16:22:25 -0300

    Projeto Bike

---
+++ projeto_bike/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <meta charset="utf-8">
+        <title>
+            Road Runner Cross Bikes SA
+        </title>
+    </head>
+    <body>
+        <!-- Cabeçalho -->
+        <header>
+            <!-- Seção Menu -->
+            <section id="secao_menu">
+                <div id="logo">
+                    
+                </div>
+                <nav id="nav_menu">
+                    <ul id="menu">
+                        <li>Home</li>
+                        <li>Notícias</li>
+                        <li>Promoções</li>
+                        <li>Sobre a Loja</li>
+                    </ul>
+                </nav>
+                <form id="caixa_form" name="frm_usuario" method="post" action="index.php">
+                    <div class="caixa_form_usuario">
+                        <label>Usuário</label>
+                        <input type="text" name="txt_usuario">
+                    </div>
+                    <div class="caixa_form_usuario">
+                        <label>Senha</label>
+                        <input type="text" name="txt_senha">
+                    </div>
+                    <div class="caixa_form_usuario">
+                        <input id="botao_form" type="submit" name="btn_ok" value="Entrar">
+                    </div>
+                </form>
+            </section>
+        </header>
+        <!-- Div Invisível atrás do Menu -->
+        <div id="menu_invisivel">
+        </div>
+        <section id="slider">
+            
+        </section>
+    </body>
+</html>
